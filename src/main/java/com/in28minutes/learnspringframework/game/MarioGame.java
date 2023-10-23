3e8662313a914fda60cf03a35eac62b89817f369
hafyze <hafizmatqwer@gmail.com>
2023-10-23 20:04:30 +0800

    feat(Tutorial-2): Loose Coupling

--- src/main/java/com/in28minutes/learnspringframework/game/MarioGame.java
+++ src/main/java/com/in28minutes/learnspringframework/game/MarioGame.java
@@ -1,20 +1,20 @@
 package com.in28minutes.learnspringframework.game;
 
-public class MarioGame {
+public class MarioGame implements GamingConsole{
 
-    public void upButton() {
+    public void up() {
         System.out.println("Jump");
     }
 
-    public void downButton() {
+    public void down() {
         System.out.println("Go into hole");
     }
 
-    public void leftButton() {
+    public void left() {
         System.out.println("Go Back");
     }
 
-    public void rightButton() {
+    public void right() {
         System.out.println("Go forward");
     }
 }
