830d0efd1c419b6f3dd1eb572cc65d0c616f6a3c
hafyze <84822836+hafyze@users.noreply.github.com>
2023-10-23 19:40:03 +0800

    feat(Tutorial-1): Getting started with Java

---
+++ src/main/java/com/in28minutes/learnspringframework/game/MarioGame.java
@@ -0,0 +1,20 @@
+package com.in28minutes.learnspringframework.game;
+
+public class MarioGame {
+
+    public void upButton() {
+        System.out.println("Jump");
+    }
+
+    public void downButton() {
+        System.out.println("Go into hole");
+    }
+
+    public void leftButton() {
+        System.out.println("Go Back");
+    }
+
+    public void rightButton() {
+        System.out.println("Go forward");
+    }
+}
