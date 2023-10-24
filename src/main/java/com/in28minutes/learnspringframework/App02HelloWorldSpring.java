e4f53c660d07574b95c09cad9e1f4c2ce3fe00ff
hafyze <hafizmatqwer@gmail.com>
2023-10-24 23:39:21 +0800

    add: more ways using Bean

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -13,6 +13,9 @@ public class App02HelloWorldSpring {
         System.out.println(context.getBean("name"));
         System.out.println(context.getBean("age"));
         System.out.println(context.getBean("person"));
-        System.out.println(context.getBean("address"));
+        System.out.println(context.getBean("person2MethodCall"));
+        System.out.println(context.getBean("person3Parameters"));
+        System.out.println(context.getBean("person_address"));
+        System.out.println(context.getBean(Address.class));
     }
 }
--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person (String name, int age) {};
+record Person (String name, int age, Address address) {};
 record Address (String address, String city) {};
 
 @Configuration
@@ -19,11 +19,21 @@ public class HelloWorldConfiguration {
     }
     @Bean
     public Person person() {
-        var person = new Person("Hafiz", 21);
+        var person = new Person("Hafiz", 21, new Address("Main Road", "Melaka"));
         return person;
     }
 
     @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address person_address) {
+        return new Person(name, age, person_address);
+    }
+
+    @Bean (name = "person_address")
     public Address address() {
         var address = new Address("23 Jalan", "Puchong");
         return address;
