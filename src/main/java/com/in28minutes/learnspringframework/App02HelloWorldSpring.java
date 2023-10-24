712cb2046374e230cc245355d8523f501cfebded
hafyze <hafizmatqwer@gmail.com>
2023-10-24 23:26:22 +0800

    step-9: Create Spring Beans
    Created in Java Spring Configuration file

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -8,7 +8,11 @@ public class App02HelloWorldSpring {
         var context =
             new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
         //2:Configure the things that we want Spring to manage - @Configuration
-        System.out.println(context.getBean("name"));
 
+        //3:Retrieving Beans managed by Spring
+        System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("address"));
     }
 }
--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,11 +3,29 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person (String name, int age) {};
+record Address (String address, String city) {};
+
 @Configuration
 public class HelloWorldConfiguration {
 
     @Bean
     public String name () {
         return "Zul";
+    }
+    @Bean
+    public int age () {
+        return 20;
+    }
+    @Bean
+    public Person person() {
+        var person = new Person("Hafiz", 21);
+        return person;
+    }
+
+    @Bean
+    public Address address() {
+        var address = new Address("23 Jalan", "Puchong");
+        return address;
     }
 }
